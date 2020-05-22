0ccae5c04f9412b825ca82233a2ec5c4c2f1dbb3
Deepti Singh <deepti.singh@i8labs.io>
2020-05-23 00:17:41 +0530

    udated dir

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,6 @@
 import boto3
 import json
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response_data = "Hi this is Hello World fom AutoCheckin"
-    return {"statusCode": 200, "body": json.dumps(response)}
+    return {"statusCode": 200, "body": json.dumps(response_data)}

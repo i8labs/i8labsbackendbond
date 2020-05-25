42d03dbbdc2b803d6ee58bfec026007ede612b2d
Deepti Singh <deepti.singh@i8labs.io>
2020-05-25 18:39:13 +0530

    testing alias

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,5 +2,5 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    response_data = "Hi this is Hello World fom AutoCheckin"
+    response_data = "Hi this is Hello World for PROD Deployment"
     return {"statusCode": 200, "body": json.dumps(response_data)}

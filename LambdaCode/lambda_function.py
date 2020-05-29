c00c2a254d4c7b0a8cb0d9feb3c54536df394a19
Deepti Singh <deepti.singh@i8labs.io>
2020-05-29 19:29:46 +0530

    Dev Commit

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,5 +2,7 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    response_data = "Hi this is Hello World for Dev-1  Deployment"
+
+    print("Function Name:", context.function_name)
+    response_data = "Hi this is Hello World for Pipeline Deployment :" + context.function_name
     return {"statusCode": 200, "body": json.dumps(response_data)}
